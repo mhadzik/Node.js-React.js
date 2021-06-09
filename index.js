0a820e0bdd6ab05d2562712afd1e45d995bb441f
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2021-06-10 01:17:27 +0200

    Update index.js file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "there", bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https://git.heroku.com/gentle-scrubland-54938.git - Heroku repo
+// https://gentle-scrubland-54938.herokuapp.com/ - Heroku app
