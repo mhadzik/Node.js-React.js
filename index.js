b3907f2d757284569501087087bbadd4546a5417
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2021-06-18 11:43:19 +0200

    add passport config for google OAuth & watch for index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there", bye: "buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("refresh token: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// https://git.heroku.com/gentle-scrubland-54938.git - Heroku repo
-// https://gentle-scrubland-54938.herokuapp.com/ - Heroku app
