bf8cccca69c4bdc6dbcadeb5ab915784ad6a1f1e
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2021-06-21 14:13:42 +0200

    config cookie based authentication and define logout & current_user routes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
@@ -9,6 +13,15 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie lasts 30 days in miliseconds
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
